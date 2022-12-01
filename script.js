1e3132662ba1c2f3ca341e19476e03e06bf5d762
Ananththandavarayamurthi <ananthcivil9876@gmail.com>
2022-12-01 14:56:13 +0530

    change

---
+++ script.js
@@ -0,0 +1,53 @@
+var counter = document.getElementById("count");
+let time = 10;
+setTimeout(() => {
+time--
+counter.innerText = time;
+
+setTimeout(() => {
+    time--
+    counter.innerText = time;
+    
+    setTimeout(() => {
+        time--
+        counter.innerText = time;
+
+        setTimeout(() => {
+            time--
+            counter.innerText = time;
+
+            setTimeout(() => {
+                time--
+                counter.innerText = time;
+
+                setTimeout(() => {
+                    time--
+                    counter.innerText = time;
+
+                    setTimeout(() => {
+                        time--
+                        counter.innerText = time;
+
+                        setTimeout(() => {
+                            time--
+                            counter.innerText = time;
+
+                            setTimeout(() => {
+                                time--
+                                counter.innerText = time;
+
+
+                                    setTimeout(() => {
+                                        time--
+                                        counter.innerText = "Happy Independence Day 🎊🎊🎊";
+    
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
